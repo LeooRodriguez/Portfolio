dbc80ded23ec9a9048cfd2491d126133e41844d6
Leonardo Rodríguez <leonardorodriguez227@gmail.com>
2025-03-06 18:56:12 -0300

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Permite la exportación estática
+  images: {
+    unoptimized: true, // Necesario para exportar imágenes sin optimización en Vercel
+  },
 };
 
 export default nextConfig;
